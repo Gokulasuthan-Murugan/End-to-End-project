82b23e467e5b83c68f3bf2a5435dbc1bb499454e
Gokulasuthan <gokulasuthan31@gmail.com>
2024-10-12 14:49:18 +0530

    commited Requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name='MLProjet',
+version='0.0.1',
+author='gokul',
+author_email='gokulasuthan55@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
